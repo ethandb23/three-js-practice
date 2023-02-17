db202209a6da39f797d8bdde64f007d6e4cbbbc7
ethandb23 <119861532+ethandb23@users.noreply.github.com>
2023-02-17 01:39:45 -1000

    set camera, zoom out

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('Hello Three.js')
+console.log(THREE)
+
+// Scene
+const scene = new THREE.Scene() 
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// ...
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+scene.add(mesh)
+
+renderer.render(scene, camera)
+
